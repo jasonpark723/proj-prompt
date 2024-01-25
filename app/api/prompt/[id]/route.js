cadb406bdf5c508685f619e026dc9ff748b38731
jasonpark723 <jasonpark723@ucla.edu>
2024-01-24 16:50:29 -0800

    fixed delete function

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -45,10 +45,11 @@ export const DELETE = async (request, { params }) => {
         await connectToDB();
 
         // Find the prompt by ID and remove it
-        await Prompt.findByIdAndRemove(params.id);
+        await Prompt.findByIdAndDelete(params.id);
 
         return new Response("Prompt deleted successfully", { status: 200 });
     } catch (error) {
+        console.error(error)
         return new Response("Error deleting prompt", { status: 500 });
     }
 };
